54d29dcaa3e9207f6de8236212f5b84f1352c48e
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-02 08:53:31 +0100

    Create city types

---
+++ src/types/city.types.ts
@@ -0,0 +1,14 @@
+type Position = {
+  lat: number;
+  lng: number;
+};
+
+export type City = {
+  cityName: string;
+  country: string;
+  emoji: string;
+  date: Date;
+  notes: string;
+  position: Position;
+  id: number;
+};
