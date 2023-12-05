63ed6b57ebbd03b4b5a0a383cad3629d3e243e57
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-05 17:57:59 +0100

    Implement useCities hook

--- src/components/city/City.tsx
+++ src/components/city/City.tsx
@@ -1,67 +1,78 @@
 import styles from "./City.module.css";
-// import { useParams, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useCities } from "hooks/useCities";
+
+import axios from "axios";
+
+import { formatDates } from "utils/functions";
 
 import { CITY } from "constants/components/city.constants";
+import { ROUTES } from "constants/components/routes.constants";
+import Spinner from "components/spinner";
+import BackButton from "components/back-button";
 
+const BASE_URL = "http://localhost:8000/";
 const WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/";
 
-const formatDate = (date: string): string | null =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long"
-  }).format(new Date(date));
-
 const City = () => {
-  // const { id } = useParams();
-  // const [searchParams, setSearchParams] = useSearchParams();
+  const { id } = useParams();
+  const { isLoading, setIsLoading, setCurrentCity, currentCity } = useCities();
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!"
+  const getCity = async (id: string) => {
+    const res = await axios.get(`${BASE_URL}${ROUTES.CITIES}/${id}`);
+    return res.data;
   };
 
-  const { cityName, emoji, date, notes } = currentCity;
+  useEffect(() => {
+    setIsLoading(true);
+    if (id) {
+      getCity(id)
+        .then((data) => setCurrentCity(data))
+        .catch((error) => console.log(error))
+        .finally(() => setIsLoading(false));
+    }
+  }, [id, setCurrentCity, setIsLoading]);
+
+  if (isLoading) return <Spinner />;
 
   return (
     <div className={styles.city}>
       <div className={styles.row}>
         <h6>{CITY.NAME}</h6>
         <h3>
-          <span>{emoji}</span> {cityName}
+          <span>{currentCity?.emoji}</span> {currentCity?.cityName}
         </h3>
       </div>
 
       <div className={styles.row}>
         <h6>
-          {CITY.YOU_WENT} {cityName} {CITY.ON}
+          {CITY.YOU_WENT} {currentCity?.cityName} {CITY.ON}
         </h6>
-        <p>{formatDate(date)}</p>
+        <p>{formatDates(currentCity?.date)}</p>
       </div>
 
-      {notes && (
+      {currentCity?.notes && (
         <div className={styles.row}>
           <h6>{CITY.YOUR_NOTES}</h6>
-          <p>{notes}</p>
+          <p>{currentCity.notes}</p>
         </div>
       )}
 
       <div className={styles.row}>
         <h6>{CITY.LEARN_MORE}</h6>
         <a
-          href={`${WIKIPEDIA_URL}${cityName}`}
+          href={`${WIKIPEDIA_URL}${currentCity?.cityName}`}
           target="_blank"
           rel="noreferrer"
         >
-          {CITY.CHECK_OUT} {cityName} {CITY.ON_WIKIPEDIA} &rarr;
+          {CITY.CHECK_OUT} {currentCity?.cityName} {CITY.ON_WIKIPEDIA} &rarr;
         </a>
       </div>
 
-      <div>{/* <ButtonBack /> */}</div>
+      <div>
+        <BackButton />
+      </div>
     </div>
   );
 };
