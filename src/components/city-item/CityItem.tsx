5ec420a0a8513c37a47bc4c55342708bec4dbde7
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-05 17:58:15 +0100

    Modify CityItem component

--- src/components/city-item/CityItem.tsx
+++ src/components/city-item/CityItem.tsx
@@ -1,9 +1,11 @@
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 import { City } from "types/city.types";
 
 import { formatDates } from "utils/functions";
+import { useCities } from "hooks/useCities";
 
 type CityProps = {
   city: City;
@@ -18,11 +20,17 @@ const CityItem = ({
     position: { lat, lng }
   }
 }: CityProps) => {
+  const { currentCity } = useCities();
+
   const queryString: string = `${id}?lat=${lat}&lng=${lng}`;
+
+  const cityItemClass = classNames(styles.cityItem, {
+    [styles["cityItem--active"]]: id === currentCity?.id
+  });
 
   return (
     <li>
-      <Link to={queryString} className={styles.cityItem}>
+      <Link to={queryString} className={cityItemClass}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDates(date)})</time>
