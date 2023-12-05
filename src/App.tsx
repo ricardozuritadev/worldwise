92396711e5e8eeef1ba9b213f9f8f4276c40508e
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-05 12:12:28 +0100

    Implemente CitiesContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState, lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import axios from "axios";
-
-import { City } from "./types/city.types";
 
 import Homepage from "./pages/homepage";
 import Product from "./pages/product";
@@ -10,6 +7,7 @@ import Pricing from "./pages/pricing";
 import Login from "./pages/login";
 import PageNotFound from "./pages/page-not-found";
 import Spinner from "./components/spinner";
+import CitiesContextProvider from "./contexts/CitiesContext";
 
 const AppLayout = lazy(() => import("./pages/app-layout"));
 const CityList = lazy(() => import("./components/city-list"));
@@ -18,74 +16,60 @@ const Form = lazy(() => import("./components/form"));
 const CityInfo = lazy(() => import("./components/city"));
 
 const App = () => {
-  const [cities, setCities] = useState<City[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const getCities = async () => {
-    const res = await axios.get("http://localhost:8000/cities");
-    return res.data;
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    getCities()
-      .then((data) => setCities(data))
-      .catch((error) => console.log(error))
-      .finally(() => setIsLoading(false));
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route
-          path="app"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <AppLayout />
-            </Suspense>
-          }
-        >
-          <Route index element={<Navigate replace to="cities" />} />
+    <CitiesContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route
-            path="cities"
+            path="app"
             element={
               <Suspense fallback={<Spinner />}>
-                <CityList isLoading={isLoading} cities={cities} />
+                <AppLayout />
               </Suspense>
             }
-          />
-          <Route
-            path="cities/:id"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <CityInfo />
-              </Suspense>
-            }
-          />
-          <Route
-            path="countries"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <CountryList isLoading={isLoading} cities={cities} />
-              </Suspense>
-            }
-          />
-          <Route
-            path="form"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <Form />
-              </Suspense>
-            }
-          />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+          >
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route
+              path="cities"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <CityList />
+                </Suspense>
+              }
+            />
+            <Route
+              path="cities/:id"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <CityInfo />
+                </Suspense>
+              }
+            />
+            <Route
+              path="countries"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <CountryList />
+                </Suspense>
+              }
+            />
+            <Route
+              path="form"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <Form />
+                </Suspense>
+              }
+            />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesContextProvider>
   );
 };
 export default App;
