a9ce572f2e5ad5141090d05e2c8bcc8683ab84df
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-05 11:26:32 +0100

    Create Map component

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,7 +1,32 @@
 import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Map = () => {
-  return <div className={styles.mapContainer}>Map container</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+
+      <button
+        onClick={() =>
+          setSearchParams({
+            lat: "23",
+            lng: "50"
+          })
+        }
+      >
+        Change position
+      </button>
+    </div>
+  );
 };
 
 export default Map;
