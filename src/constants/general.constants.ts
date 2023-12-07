8a652384765089a2437ebe1e4bc1c06230a641dd
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-07 13:57:00 +0100

    Add general constant

--- src/constants/general.constants.ts
+++ src/constants/general.constants.ts
@@ -3,5 +3,6 @@ export const GENERAL = {
   WORDLWISE: "WorldWise Inc.",
   MAP: "Map",
   POSITION: "Position",
-  PAGE_NOT_FOUND: "Page not found"
+  PAGE_NOT_FOUND: "Page not found",
+  NO_GEOLOCATION_SUPPORT: "Your browser does not support geolocation"
 };
