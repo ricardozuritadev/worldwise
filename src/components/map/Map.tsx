0390276fdb6ad8101d3955e2aa68c06e1bac176d
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2023-12-07 13:56:48 +0100

    Implement leaflet map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,34 +1,98 @@
 import styles from "./Map.module.css";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "hooks/useCities";
+import { useGeolocation } from "hooks/useGeoLocation";
 
-import { GENERAL } from "constants/general.constants";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents
+} from "react-leaflet";
+
+import { LatLngExpression, LeafletMouseEvent } from "leaflet";
+import Button from "components/button";
+import { ButtonType } from "types/button.types";
 
 const Map = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState<LatLngExpression>([40, 0]);
+  const { cities } = useCities();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition
+  } = useGeolocation();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = Number(searchParams.get("lat"));
+  const mapLng = Number(searchParams.get("lng"));
+
+  useEffect(() => {
+    if (mapLat != 0 && mapLng != 0) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
+
+  useEffect(() => {
+    if (geoLocationPosition)
+      setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>{GENERAL.MAP}</h1>
-      <h2>
-        {GENERAL.POSITION}: {lat}, {lng}
-      </h2>
-
-      <button
-        onClick={() =>
-          setSearchParams({
-            lat: "23",
-            lng: "50"
-          })
-        }
+    <div className={styles.mapContainer}>
+      {!geoLocationPosition && (
+        <Button type={ButtonType.Position} onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map(({ id, position, emoji, cityName }) => (
+          <Marker position={position} key={id}>
+            <Popup>
+              <span>{emoji}</span>
+              <span>{cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+};
+
+type ChangeCenterProps = {
+  position: LatLngExpression;
+};
+
+const ChangeCenter = ({ position }: ChangeCenterProps) => {
+  const map = useMap();
+  map.setView(position);
+
+  return null;
+};
+
+const DetectClick = () => {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e: LeafletMouseEvent) => {
+      navigate(`form?lat=${e.latlng.lat}&long=${e.latlng.lng}`);
+    }
+  });
+
+  return null;
 };
 
 export default Map;
