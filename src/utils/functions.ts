dc9fd2c415373fd1b174dac4fb9f16b90bf47112
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-14 17:52:48 +0100

    Create convertToEmoji function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,9 +1,19 @@
 export const formatDates = (date: Date | undefined) => {
-  if (!date) return "No date";
+  if (!date) return 'No date';
 
-  return new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric"
+  return new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
   }).format(new Date(date));
 };
+
+export function convertToEmoji(countryCode: string) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map((char) => {
+      return 127397 + char.charCodeAt(0);
+    });
+  return String.fromCodePoint(...codePoints);
+}
