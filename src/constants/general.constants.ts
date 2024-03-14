8c7718897a21a94e16e4084be52b6f37a0a00578
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-14 19:38:31 +0100

    Add constants

--- src/constants/general.constants.ts
+++ src/constants/general.constants.ts
@@ -5,5 +5,6 @@ export const GENERAL = {
   POSITION: "Position",
   PAGE_NOT_FOUND: "Page not found",
   NO_GEOLOCATION_SUPPORT: "Your browser does not support geolocation",
-  NO_COUNTRY_CLICKED: "That doesn't seem to be a city. Click somewhere else"
+  NO_COUNTRY_CLICKED: "That doesn't seem to be a city. Click somewhere else",
+  NO_LAT_LONG: "Start by clicking somewhere on the map"
 };
