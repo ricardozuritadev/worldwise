dd43f19dc0a880d88cac07b0977b357e042ca1fc
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-14 19:38:08 +0100

    Implement deleteCity function

--- src/components/city-item/CityItem.tsx
+++ src/components/city-item/CityItem.tsx
@@ -1,4 +1,6 @@
 import styles from "./CityItem.module.css";
+
+import { MouseEvent } from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 
@@ -20,7 +22,7 @@ const CityItem = ({
     position: { lat, lng }
   }
 }: CityProps) => {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
 
   const queryString: string = `${id}?lat=${lat}&lng=${lng}`;
 
@@ -28,13 +30,20 @@ const CityItem = ({
     [styles["cityItem--active"]]: id === currentCity?.id
   });
 
+  function handleClick(e: MouseEvent<HTMLElement>) {
+    e.preventDefault();
+    if (id) deleteCity(id);
+  }
+
   return (
     <li>
       <Link to={queryString} className={cityItemClass}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDates(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
