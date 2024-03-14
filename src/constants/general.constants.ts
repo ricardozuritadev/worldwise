5ad0c993afe87f17c86b1f60e7dcf83338f1e054
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-14 17:54:13 +0100

    Create error constant

--- src/constants/general.constants.ts
+++ src/constants/general.constants.ts
@@ -1,8 +1,9 @@
 export const GENERAL = {
-  COPYRIGHT: "Copyright",
-  WORDLWISE: "WorldWise Inc.",
-  MAP: "Map",
-  POSITION: "Position",
-  PAGE_NOT_FOUND: "Page not found",
-  NO_GEOLOCATION_SUPPORT: "Your browser does not support geolocation"
+  COPYRIGHT: 'Copyright',
+  WORDLWISE: 'WorldWise Inc.',
+  MAP: 'Map',
+  POSITION: 'Position',
+  PAGE_NOT_FOUND: 'Page not found',
+  NO_GEOLOCATION_SUPPORT: 'Your browser does not support geolocation',
+  NO_COUNTRY_CLICKED: "That doesn't seem to be a city. Click somewhere else"
 };
